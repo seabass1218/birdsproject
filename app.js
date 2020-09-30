0fa7b302d0d9fe59e3eeb219f920269ca1e99f44
Christian Fast <cfast@gus.pittstate.edu>
2020-09-30 16:40:10 -0500

    fix setting page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var settingsRouter=require('./routes/settings');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/settings', settingsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/settings.js
+++ routes/settings.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'settings' });
 });
 
 /* GET home page. */
 router.get('/birds', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'birds' });
   });
 
 /* GET home page. */
 router.get('/meds', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'meds' });
   });
   
 
