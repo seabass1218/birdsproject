5505ae4bb8cbe916edeba1409a3d78b63030cf02
sebastian loyd <sloyd@gus.pittstate.edu>
2020-10-14 21:38:09 -0500

    added some pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/birds', function(req, res, next) {
+  res.render('birds', { title: 'Birds' });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create', { title: 'Create' });
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('users', { title: 'Users' });
 });
 
 module.exports = router;
