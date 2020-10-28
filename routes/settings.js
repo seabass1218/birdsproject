435a69ac870edc4d38a2238de692ac05c5ee5f09
sebastian loyd <sloyd@gus.pittstate.edu>
2020-10-28 10:44:13 -0500

    created forms for settings pages

--- routes/settings.js
+++ routes/settings.js
@@ -16,6 +16,18 @@ router.get('/meds', function(req, res, next) {
     res.render('meds', { title: 'meds' });
   });
 
+router.get('/meds/create', function(req, res, next) {
+    res.render('createMeds', { title: 'meds' });
+  }); 
+
+router.get('/users/create', function(req, res, next) {
+    res.render('createUsers', { title: 'meds' });
+  }); 
+
+router.get('/food/create', function(req, res, next) {
+    res.render('createFood', { title: 'meds' });
+  }); 
+
 router.get('/food', function(req, res, next) {
     res.render('food', { title: 'food' });
   });
