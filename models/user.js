5d093ee64a6fa2e19522a3a79a94221148679738
sebastian loyd <sloyd@gus.pittstate.edu>
2020-11-09 11:44:36 -0600

    updated schema, added password validation and hash generation

--- models/user.js
+++ models/user.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
-  email: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   registerDate: {type: Date, default: Date.now,required: true},
   role: {type: String, required: true},
 });
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checks if password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
